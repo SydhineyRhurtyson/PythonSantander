b4334f1c739b8fa5837025cb1f5aac90600e1a98
Sydhiney Rhurtyson <106470993+SydhineyRhurtyson@users.noreply.github.com>
2026-09-06 21:03:22 -0300

    loops

---
+++ loops.py
@@ -0,0 +1,13 @@
+alunos = ["Victor","Maria","Eduarda","Danieli"]
+
+for anulo in alunos:
+    print(anulo)
+
+
+print(40*"=")
+from time import sleep
+contador = 10
+while contador > 0:
+    print(contador)
+    contador -=1
+    sleep(0.5)
